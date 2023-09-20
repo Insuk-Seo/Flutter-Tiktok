89c3a17def8ce8d861767d3048d2fd81b9259090
Insuk <insukvomn@unist.ac.kr>
2023-09-20 11:23:49 +0900

    4.3 Sign Up Form

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,19 @@ class TikTokApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tiktok Clone',
       theme: ThemeData(
+        scaffoldBackgroundColor:
+            Colors.white, // scaffold를 전역적으로 바꿔줄 수  있음. 기본 테마
         primaryColor: const Color(0xFFE9435A),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            fontSize: Sizes.size16 + Sizes.size2,
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
+          ),
+        ), // AppBarTheme를 이용하여 AppBar의 전역적인 테마를 바꿔줄 수 있음.
       ),
       home: const SignUpScreen(),
     );
